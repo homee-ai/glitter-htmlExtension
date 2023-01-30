e9f0f5fa83831eb163793cb57e7a7d41c6a8fd41
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-01-30 12:21:13 +0800

    1675052473429

--- src/empty.js
+++ src/empty.js
@@ -2,6 +2,48 @@
 (() => {
     const glitter = window.glitter;
     const obj = {
+        title: {
+            defaultData: {
+                font: "12345"
+            },
+            render: (gvc, widget, setting) => {
+                var _a, _b, _c;
+                return {
+                    view: `<h3 style="color: ${(_a = widget.data.color) !== null && _a !== void 0 ? _a : "black"};font-size: ${(_b = widget.data.font) !== null && _b !== void 0 ? _b : ""}px;">${widget.data.title}</h3>`,
+                    editor: gvc.map([
+                        glitter.htmlGenerate.editeInput({
+                            gvc: gvc,
+                            title: "標題",
+                            default: widget.data.title,
+                            placeHolder: "請帶入標題",
+                            callback: (text) => {
+                                widget.data.title = text;
+                                widget.refreshAll();
+                            }
+                        }),
+                        `
+<h3 style="color: white;font-size: 16px;margin-bottom: 10px;" class="mt-2">字型大小</h3>
+<input class="form-control" placeholder="" onchange="${gvc.event((e) => {
+                            widget.data.font = e.value;
+                            widget.refreshAll();
+                        })}" value="${(_c = widget.data.font) !== null && _c !== void 0 ? _c : ""}">
+                            `,
+                        (() => {
+                            const option = ['red', 'black', 'yellow', 'blue'];
+                            return `<h3 style="color: white;font-size: 16px;margin-bottom: 10px;" class="mt-2">字型顏色</h3>
+                            <select class="form-control" onchange="${gvc.event((e) => {
+                                widget.data.color = e.value;
+                                widget.refreshAll();
+                            })}">
+                            ${gvc.map(option.map((dd) => {
+                                return `<option value="${dd}" ${(dd === widget.data.color) ? `selected` : ``}>${dd}</option>`;
+                            }))}
+</select>`;
+                        })()
+                    ])
+                };
+            }
+        },
         banner: {
             defaultData: {},
             render: (gvc, widget, setting) => {
