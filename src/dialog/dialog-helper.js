82060a7e8418475e07bde18113305bc62b553a91
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-02-04 15:07:22 +0800

    1675494442761

--- src/dialog/dialog-helper.js
+++ src/dialog/dialog-helper.js
@@ -2,7 +2,7 @@ export class DialogHelper {
     static dataLoading(obj) {
         const glitter = window.glitter;
         if (obj.visible) {
-            glitter.openDiaLog('dialog/dialog.js', 'dataLoading', { type: 'dataLoading', obj: obj });
+            glitter.openDiaLog(new URL('./dialog.js',import.meta.url), 'dataLoading', { type: 'dataLoading', obj: obj });
         }
         else {
             glitter.closeDiaLog('dataLoading');
