62c6a9ebd57b3eaa08436804639b91326f6f1c0f
homee-Zack <jen-hung.lai@homee.ai>
2023-02-21 12:12:37 +0800

    0221

---
+++ src/idea/idea.js
@@ -0,0 +1,53 @@
+'use strict';
+import { Plugin } from '../glitterBundle/plugins/plugin-creater.js';
+import { Api } from "../homee/api/homee-api.js";
+Plugin.create(import.meta.url, (glitter) => {
+    const api = {
+        upload: (photoFile, callback) => {
+            glitter.share.dialog.dataLoading({ text: '上傳中', visible: true });
+            $.ajax({
+                url: Api.serverURL + '/api/v1/scene/getSignedUrl',
+                type: 'post',
+                data: JSON.stringify({ file_name: `${new Date().getTime()}` }),
+                contentType: 'application/json; charset=utf-8',
+                headers: { Authorization: glitter.getCookieByName('token') },
+                success: (data1) => {
+                    $.ajax({
+                        url: data1.url,
+                        type: 'put',
+                        data: photoFile,
+                        processData: false,
+                        crossDomain: true,
+                        success: (data2) => {
+                            glitter.share.dialog.dataLoading({ visible: false });
+                            glitter.share.dialog.successMessage({ text: "上傳成功" });
+                            callback(data1.fullUrl);
+                        },
+                        error: (err) => {
+                            glitter.share.dialog.successMessage({ text: "上傳失敗" });
+                        },
+                    });
+                },
+                error: (err) => {
+                    glitter.share.dialog.successMessage({ text: "上傳失敗" });
+                },
+            });
+        }
+    };
+    return {
+        banner: {
+            defaultData: {
+                link: []
+            },
+            render: (gvc, widget, setting, hoverID) => {
+                const data = widget.data;
+                return {
+                    view: () => { return ``; },
+                    editor: () => {
+                        return ``;
+                    }
+                };
+            },
+        },
+    };
+});
---
+++ src/idea/idea.ts
@@ -0,0 +1,56 @@
+'use strict';
+import {Plugin} from '../glitterBundle/plugins/plugin-creater.js'
+import {Api} from "../homee/api/homee-api.js";
+
+Plugin.create(import.meta.url,(glitter)=>{
+    const api={
+        upload:(photoFile:any,callback:(link:string)=>void)=>{
+            glitter.share.dialog.dataLoading({text:'上傳中',visible:true})
+            $.ajax({
+                url: Api.serverURL+'/api/v1/scene/getSignedUrl',
+                type: 'post',
+                data: JSON.stringify({ file_name:`${new Date().getTime()}`}),
+                contentType: 'application/json; charset=utf-8',
+                headers: { Authorization: glitter.getCookieByName('token') },
+                success: (data1: { url: string; fullUrl: string }) => {
+                    $.ajax({
+                        url: data1.url,
+                        type: 'put',
+                        data: photoFile,
+                        processData: false,
+                        crossDomain: true,
+                        success: (data2: any) => {
+                            glitter.share.dialog.dataLoading({visible:false})
+                            glitter.share.dialog.successMessage({text:"上傳成功"})
+                            callback(data1.fullUrl)
+                        },
+                        error: (err: any) => {
+                            glitter.share.dialog.successMessage({text:"上傳失敗"})
+                        },
+                    });
+                },
+                error: (err: any) => {
+                    glitter.share.dialog.successMessage({text:"上傳失敗"})
+                },
+            });
+        }
+    }
+    return {
+        banner: {
+            defaultData:{
+                link:[]
+            },
+            render:(gvc, widget, setting, hoverID) => {
+                const data: { link: { img: string,code?:string }[] } = widget.data
+
+                return {
+                    view: ()=>{return ``},
+                    editor: ()=>{
+                        return ``
+                    }
+                }
+            },
+        },
+
+    }
+});
