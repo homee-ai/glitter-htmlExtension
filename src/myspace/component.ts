b34bceefebd9e24f3eefc049323936b23a7ab9a9
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-02-22 02:14:40 +0800

    fix

--- src/myspace/component.ts
+++ src/myspace/component.ts
@@ -2,8 +2,3 @@
 import {Plugin} from '../glitterBundle/plugins/plugin-creater.js'
 import {ClickEvent} from "../glitterBundle/plugins/click-event.js";
 import {appConfig} from "../config.js";
-
-// @ts-ignore
-Plugin.create(import.meta.url,(glitter)=>{
-
-})
