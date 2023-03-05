ab5e913739a71e1ce6101d97719b3f6ec08cabaa
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-03-05 23:06:55 +0800

    fix login

--- src/homee/event.ts
+++ src/homee/event.ts
@@ -103,6 +103,100 @@ ClickEvent.create(import.meta.url, {
             }
         }
     },
+    pageSwitchNeedLogin: {
+        title: "頁面跳轉-需要登入",
+        fun: (gvc, widget, object: { selectPage: { tag?: string, name?: string } }) => {
+            return {
+                editor: () => {
+                    const vm: {
+                        loading: boolean,
+                        data: { name: string, tag: string }[]
+                    } = {
+                        loading: true,
+                        data: []
+                    }
+                    const id = gvc.glitter.getUUID()
+                    const api = new Api()
+                    object.selectPage = object.selectPage ?? {}
+                    api.homeeAJAX({
+                        api: Api.serverURL,
+                        route: '/api/v1/lowCode/pageConfig?query=tag,`group`,name',
+                        method: 'get'
+                    }, (res) => {
+                        vm.data = res.result
+                        vm.loading = false
+                        gvc.notifyDataChange(id)
+                    })
+                    return `
+                    <h3 class="m-0 mb-2 mt-2" style="font-size: 16px;">選擇頁面</h3>
+                    ${
+                        gvc.bindView(() => {
+                            return {
+                                bind: id,
+                                view: () => {
+                                    if (vm.loading) {
+                                        return `<option value='${JSON.stringify(object.selectPage)}'>${object.selectPage.name ?? "尚未選擇"}</option>`
+                                    }
+                                    let haveData = false
+                                    return gvc.map(vm.data.map((dd) => {
+                                        haveData = haveData || object.selectPage.tag === dd.tag
+                                        return `<option value='${JSON.stringify(dd)}' ${(object.selectPage.tag === dd.tag) ? `selected` : ``}>${dd.name}</option>`
+                                    })) + ((haveData) ? `` : `<option selected>尚未定義</option>`)
+                                },
+                                divCreate: {
+                                    class: `form-control`, elem: `select`, option: [
+                                        {
+                                            key: 'onChange',
+                                            value: gvc.event((e, event) => {
+                                                object.selectPage = JSON.parse(e.value)
+                                                widget.refreshAll()
+                                            })
+                                        }
+                                    ]
+                                }
+                            }
+                        })
+                    }
+                    `
+                },
+                event: () => {
+                    appConfig().getUserData({
+                        callback: (userData) => {
+                            try {
+                                const dialog = new Dialog(gvc)
+                                dialog.dataLoading(true)
+                                if (userData.token) {
+                                    User.checkToken(userData.token, (response) => {
+                                        if (response) {
+                                            dialog.dataLoading(false)
+                                            appConfig().changePage(gvc, object.selectPage.tag!)
+                                        } else {
+                                            User.login({
+                                                account: userData.user_id,
+                                                pwd: userData.pwd,
+                                                callback: (response) => {
+                                                    dialog.dataLoading(false)
+                                                    if (response) {
+                                                        appConfig().changePage(gvc, object.selectPage.tag!)
+                                                    } else {
+                                                        appConfig().changePage(gvc, object.selectPage.tag!)
+                                                    }
+                                                }
+                                            })
+                                        }
+                                    })
+                                } else {
+                                    appConfig().changePage(gvc, object.selectPage.tag!)
+                                }
+                            } catch (e) {
+                                appConfig().changePage(gvc, object.selectPage.tag!)
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    },
     setHome: {
         title: "首頁設定",
         fun: (gvc, widget, object: { selectPage: { tag?: string, name?: string } }) => {
