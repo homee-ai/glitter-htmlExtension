1152fd6cca205415d64c34e1466bacf5a3570dfb
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-03-08 16:02:17 +0800

    fix ui

--- src/order/order.ts
+++ src/order/order.ts
@@ -35,7 +35,7 @@ Plugin.create(import.meta.url, (glitter, editMode) => {
                 const vm = {
                     loading: true
                 }
-                if (editMode) {
+                if (!editMode) {
                     vm.loading = true
                     Checkout.getOrderList({
                         callback: (result) => {
