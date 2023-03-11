b17621195808e74b062ec9dd946862b11fb9d20b
ZiZiHo <38238578+ZiZiHo@users.noreply.github.com>
2023-03-11 12:47:55 +0800

    fix myspaceProduct detail

--- src/myspace/component.ts
+++ src/myspace/component.ts
@@ -495,6 +495,7 @@ ${glitter.htmlGenerate.editeInput({
         },
         productDetail: {
             defaultData: {
+                "orgSelectSku":"",
                 "qty": 1,
                 "intro": [{
                     "text": "<p>OLVAN 檯燈外形簡約優雅，能為您的睡房添加充滿格調，同時具有極佳質感的照明單品。</p>\n<div style=\"text-align: center;\" data-mce-style=\"text-align: center;\"><img style=\"margin-bottom: 14px; float: none;\" alt=\"\" src=\"https://cdn.shopify.com/s/files/1/0704/0158/9548/files/8842090_1024x1024.jpg?v=1675167211\" data-mce-src=\"https://cdn.shopify.com/s/files/1/0704/0158/9548/files/8842090_1024x1024.jpg?v=1675167211\" data-mce-style=\"margin-bottom: 14px; float: none;\"></div>\n<p>OLVAN 檯燈底座採用實木材料，具有優良出色的穩固承重力，整體厚實穩固。棉布或百頁材質燈罩讓燈光更柔和，輕鬆營造朦朧浪漫的氛圍，為你快節奏的生活中帶來一些平靜和溫柔，別有一番浪漫情懷。</p>\n<div style=\"text-align: center;\" data-mce-style=\"text-align: center;\"><img style=\"margin-bottom: 14px; float: none;\" alt=\"\" src=\"https://cdn.shopify.com/s/files/1/0704/0158/9548/files/8842091_1024x1024.jpg?v=1675167329\" data-mce-src=\"https://cdn.shopify.com/s/files/1/0704/0158/9548/files/8842091_1024x1024.jpg?v=1675167329\" data-mce-style=\"margin-bottom: 14px; float: none;\"></div>\n<p>OLVAN 檯燈有多重款式任你選擇，很適合自用或作為禮品送給親友。HOMEE 成就每一個明亮溫馨的家，與您一同構建理想中的質感生活！</p>\n<p> </p>\n<p> </p>\n<h4>商品規格</h4>\n<table width=\"100%\">\n<tbody>\n<tr>\n<td style=\"width: 30%; text-align: center; background-color: #efefef;\" data-mce-style=\"width: 30%; text-align: center; background-color: #efefef;\">\n<p><strong>直徑</strong></p>\n</td>\n<td style=\"width: 70%;\" data-mce-style=\"width: 70%;\">\n<p>棉布款：26 公分，百頁：24 公分</p>\n</td>\n</tr>\n<tr>\n<td style=\"width: 30%; text-align: center; background-color: #efefef;\" data-mce-style=\"width: 30%; text-align: center; background-color: #efefef;\">\n<p><strong>寬</strong></p>\n</td>\n<td style=\"width: 70%;\" data-mce-style=\"width: 70%;\">\n<p>32 公分</p>\n</td>\n</tr>\n</tbody>\n</table>\n<br>\n<table width=\"100%\">\n<tbody>\n<tr>\n<td style=\"width: 30%; text-align: center; background-color: #efefef;\" data-mce-style=\"width: 30%; text-align: center; background-color: #efefef;\">\n<p data-mce-style=\"text-align: left;\"><strong>材質說明</strong></p>\n</td>\n<td data-mce-style=\"width: 70%;\">\n<p>底座：原木，燈罩：棉布 / PVC</p>\n</td>\n</tr>\n</tbody>\n</table>\n<p> </p>\n<p> </p>\n<h4>相關說明</h4>\n<p>※ 訂購前敬請詳閱<a href=\"https://homee.cc/legal/refund-policy\" target=\"_blank\">退換貨說明</a>，您送出訂單的同時將視同您已詳閱、同意以下規定。</p>\n<p>※ 顏色差異：商品顏色或布料會因您觀看的裝置 (手機、平板或電腦等) 而無法100%相同，商品顏色與布料以實品為主。</p>\n<p>※ 尺寸誤差：所有產品尺寸皆為人工丈量，可能因測量不同而存在誤差，0.5 - 2 公分 屬正常。</p>\n<p>※ 溫馨提醒：家具類商品長途運輸難免有碰撞風險，HOMEE 將盡力協助您服務至商品完善，您可放心選購。</p>\n<p>※ 商品交期：HOMEE 提供平台讓您使用合宜價錢直接向供應廠採購因此交期會因工廠有無現貨而有異動，有現貨約 1 週配送，無現貨需等約 2~4 週。 </p>",
@@ -634,16 +635,6 @@ ${glitter.htmlGenerate.editeInput({
                     const num = Number(numStr);
                     return num.toLocaleString();
                 }
-                function qtyChange(action: boolean = true) {
-                    widget.data.qty += (action) ? 1 : -1;
-
-                    if (widget.data.qty < 0) {
-                        widget.data.qty = 0
-                    }
-
-
-                    gvc.notifyDataChange("qtyNumber");
-                }
                 function goToSlide(index: number): void {
                     const Swiper = (window as any).Swiper
                     let mySwiper = new Swiper('.swiper', {
@@ -679,7 +670,8 @@ ${glitter.htmlGenerate.editeInput({
                             </div>`
                         }
                         let sku_list = (widget.data.productData && widget.data.productData.sku_list) ?? {}
-                        let key: string[] = []
+                        let key: string[] = [];
+                        let btnChange = false;
                         widget.data.attribute_list?.map((dd: any) => {
                             const select = dd.attribute_values.find((d2: any) => {
                                 return d2.selected
@@ -687,6 +679,13 @@ ${glitter.htmlGenerate.editeInput({
                             select && key.push(select.value)
                         })
                         const selectSku = sku_list[key.join(' / ')]
+
+                        console.log("選擇")
+                        console.log(widget.data.orgSelectSku)
+                        if (!widget.data.orgSelectSku){
+                            console.log("test")
+                            widget.data.orgSelectSku = selectSku;
+                        }
                         setTimeout(()=>{
                             goToSlide(selectSku.image_index);
                         },250)
@@ -715,6 +714,7 @@ ${glitter.htmlGenerate.editeInput({
                                         ${gvc.bindView({
                                         bind: `type${index}`,
                                         view: () => {
+                                            
                                             return `
                                                 <div class="sizeSelectTitle">
                                                     ${sizeType.attribute_key}
@@ -722,18 +722,22 @@ ${glitter.htmlGenerate.editeInput({
 
                                                 <div class="d-flex flex-wrap" style="overflow: scroll;padding: 8px;">
                                                     ${gvc.map(sizeType.attribute_values.map((data: any, index: number) => {
+                                                
                                                 let className = "kindUnselected"
                                                 if (data.selected) {
                                                     className += " kindSelected"
                                                 }
                                                 return `
                                                     <div class="${className}" style="margin-top: 8px;" onclick="${gvc.event(() => {
-                                                    sizeType.attribute_values.map((dd: any) => {
-                                                        dd.selected = false
-                                                    })
-                                                    data.selected = true
-                                                    widget.refreshComponent()
-                                                })}">${data.value}
+                                                        sizeType.attribute_values.map((dd: any) => {
+                                                            dd.selected = false
+                                                        })
+                                                        data.selected = true
+                                                        
+                                                        
+                                                        
+                                                        widget.refreshComponent()
+                                                    })}">${data.value}
                                                     </div>
                                                         `
                                             }))}
@@ -756,12 +760,36 @@ ${glitter.htmlGenerate.editeInput({
                             }, divCreate: {class: ``, style: "padding:0 24px 32px;"},
 
                         })}
-                       
-                        <div class="w-100 d-flex  flex-fill " style="padding: 0 59px;height: 48px;">
-                            <div class="d-flex align-items-center justify-content-center flex-fill" style="background: #FE5541;border-radius: 24px;font-weight: 700;font-size: 18px;line-height: 26px;text-align: center;letter-spacing: 0.15em;color: #FFFFFF;" onclick="${gvc.event(()=>{
-                                glitter.runJsInterFace("addToSpace",selectSku,()=>{})
-                            })}">加入至空間</div>                                        
-                        </div>
+                        ${gvc.bindView({
+                            bind:"BTN",
+                            view:()=>{
+                                if (widget.data.orgSelectSku!= selectSku){
+                                    return `
+                                    <div class="w-100 d-flex  flex-fill " style="padding: 0 59px;height: 48px;">
+                                        <div class="d-flex align-items-center justify-content-center flex-fill" style="background: #FE5541;border-radius: 24px;font-weight: 700;font-size: 18px;line-height: 26px;text-align: center;letter-spacing: 0.15em;color: #FFFFFF;" onclick="${gvc.event(()=>{
+                                        glitter.runJsInterFace("addToSpace",selectSku,()=>{
+
+                                        })
+                                    })}">更換規格</div>                                        
+                                    </div>
+                                `
+                                }else {
+                                    return `
+                                    <div class="w-100 d-flex  flex-fill " style="padding: 0 59px;height: 48px;">
+                                        <div class="d-flex align-items-center justify-content-center flex-fill" style="background: #E0E0E0;border-radius: 24px;font-weight: 700;font-size: 18px;line-height: 26px;text-align: center;letter-spacing: 0.15em;color: #858585;" onclick="${gvc.event(()=>{
+                                        glitter.runJsInterFace("addToSpace",selectSku,()=>{
+
+                                        })
+                                    })}">已加入至空間</div>                                        
+                                    </div>
+                                `
+                                }
+                            },
+                            divCreate:{}
+                            
+                        })}
+           
+                        
                         
                         ${gvc.bindView({
                             bind: `intro`,
