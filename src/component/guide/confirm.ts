55ef41e6b276d30b0a83e3c1adb5fb6ed823e5c1
ZiZiHo <38238578+ZiZiHo@users.noreply.github.com>
2023-03-25 20:13:50 +0800

    adjust checkbox color

--- src/component/guide/confirm.ts
+++ src/component/guide/confirm.ts
@@ -34,14 +34,16 @@ init((gvc, glitter, gBundle) => {
                     font-weight: normal;
                 }
                 #confirm-appear::before {
-                    content: "";
+                    content: "✔";
                     display: inline-block;
-                    width: 15px;
-                    height: 15px;
-                    border: 1px solid #1E1E1E;
-                  
-                  
-                  
+                    width: 17px;
+                    height: 17px;
+                    text-align: center;
+                    color:white;
+                    font-size:15px;                   
+                    border-radius: 4px;
+                    margin:-1px -1px;
+                    
                 }
                 #confirm-appear:checked::before {
                   background-color: #FE5541;
@@ -51,11 +53,11 @@ init((gvc, glitter, gBundle) => {
                 <div class="w-100 d-flex justify-content-center align-items-center" style="position: fixed;top: 50%;left:50%;transform: translate(-50%, -50%);">
                     <div style="width:288px;font-family: 'Noto Sans TC',serif;font-style: normal;font-feature-settings: 'pnum' on, 'lnum' on;font-size: 14px;line-height: 150%;text-align: center;background: #FFFFFF;box-shadow: -3px 3px 15px rgba(0, 0, 0, 0.1);border-radius: 15px;padding-top: 18px;">
                         <div style="font-weight: 700;font-size: 18px;line-height: 26px;color: #1E1E1E;">HOMEE</div>
-                        <div style="font-weight: 700;font-size: 18px;line-height: 26px;color: #1E1E1E;">確定退出教學影片嗎?</div>
+                        <div style="font-weight: 400;font-size: 18px;line-height: 26px;color: #1E1E1E;">確定退出教學影片嗎?</div>
                         <div class="d-flex align-items-center justify-content-center w-100" style="margin-top: 8px;">
-                            <input style="appearance: none;width: 16px;height: 16px;margin-right:8px;font-weight: 400;font-size: 12px;line-height: 17px;color: #1E1E1E;" type="checkbox" id="confirm-appear" >不再顯示教學影片
+                            <input style="border-radius: 4px;border: 1px solid #1E1E1E;appearance: none;width: 16px;height: 16px;margin-right:8px;font-weight: 400;font-size: 12px;line-height: 17px;color: #1E1E1E;" type="checkbox" id="confirm-appear" >不再顯示教學影片
                         </div>
-                        <div class="d-flex align-items-center" style="margin-top: 18px;border-top: 1px solid #E0E0E0;font-weight: 400;font-size: 18px;line-height: 26px;color: #FE5541;">
+                        <div class="d-flex align-items-center" style=" margin-top: 18px;border-top: 1px solid #E0E0E0;font-weight: 400;font-size: 18px;line-height: 26px;color: #FE5541;">
                             <div class="w-50" style="height: 100%" onclick="${gvc.event(()=>{
                                 glitter.closeDiaLog("leaveGuide")
                             })}">取消</div>
