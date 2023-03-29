faa986e27f80a6827be50b46dd4e4e34546e90f3
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-03-30 04:16:19 +0800

    fix ui

--- src/myspace/component.ts
+++ src/myspace/component.ts
@@ -33,32 +33,7 @@ Plugin.create(import.meta.url, (glitter) => {
                                 data: [],
                                 showMore: true
                             }
-                            function checkFirstInto(){
-                                Myspace.getFirstView((res:any)=>{
-                                    if (res.result == "SUCCESS"){
-                                        if (res.watched){
-                                            viewGuide = res.watched;
-                                        }else {
-                                            glitter.getPro("tempLeave" ,(data:any)=>{
-                                                console.log("TETET")
-                                                console.log(data)
-                                                if (data.data=="true"){
-                                                    viewGuide = true;
-                                                    gvc.notifyDataChange("coverGuide");
-                                                }else {
-                                                    viewGuide = false;
-                                                    gvc.notifyDataChange("coverGuide");
-                                                }
-                                            })
-                                        }
 
-                                    }else {
-
-                                        alert("請檢察登入狀態或是網路資訊")
-                                    }
-                                })
-                            }
-                            checkFirstInto();
 
                             function getData() {
                                 vm.data =[]
